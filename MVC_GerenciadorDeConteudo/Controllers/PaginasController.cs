395be1f43a3d5fbbe9977614d1e7c2eb85b99644
fernandaos12 <fernandaos12@gmail.com>
2021-11-22 22:09:14 -0300

    camada ui

---
+++ MVC_GerenciadorDeConteudo/Controllers/PaginasController.cs
@@ -0,0 +1,26 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_GerenciadorDeConteudo.Controllers
+{
+    public class PaginasController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+       
+    }
+}
