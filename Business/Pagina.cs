3b25ea24e4f2d3816d68f7eb8de0b66e6b60710f
fernandaos12 <fernandaos12@gmail.com>
2021-11-22 22:02:44 -0300

    construindo camadas mvc

---
+++ Business/Pagina.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Pagina
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Conteudo  { get; set; }
+        public DateTime Data { get; set; }
+
+
+        public List<Pagina> Lista()
+        {
+            var Lista = new List<Pagina>();
+            var paginaDb = new Database.Pagina();
+            
+                foreach(DataRow row in paginaDb.Lista().Rows)
+                {
+                var pagina = new Pagina();
+                pagina.Id = Convert.ToInt32(row["Id"]);
+                pagina.Name = Convert.ToString(row["Nome"]);
+                pagina.Conteudo = row["conteudo"].ToString();
+                pagina.Data = Convert.ToDateTime(row["data"]);
+
+                
+            }
+           return Lista;
+        }
+
+    }
+}
---
+++ Database/Pagina.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Pagina
+    {
+        private string SqlConn()
+        {
+            return ConfigurationManager.AppSettings["sqlConn"];
+        }
+
+        public DataTable Lista()
+        {
+            using (SqlConnection conn = new SqlConnection(SqlConn()))
+            {
+                string queryString = "SELECT * FROM paginas";
+                SqlCommand command = new SqlCommand(queryString, conn);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = command;
+
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                return table;
+            }
+           
+        }
+
+    }
+}
